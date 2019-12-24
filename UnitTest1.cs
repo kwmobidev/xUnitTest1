b46040c55e8a4891ec54c5a3dcf7de26aa9bbfe5
kwmobidev <meyertanuan@gmail.com>
2019-12-24 10:00:38 -0500

    BTN-1123

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,9 +6,24 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void xUnitWorks()
         {
-
+            Assert.True(true);
         }
+
+        [Fact]
+        public void WhenAddTwoNumberThenSumIsCorrect()
+        {
+            var sum = 1 + 2;
+            Assert.Equal(3, sum);
+        }
+
+        [Fact]
+        public void WhenSubtractTwoNumberThenDifferenceIsCorrect()
+        {
+            var diff = 5 - 2;
+            Assert.Equal(3, diff);
+        }
+
     }
 }
---
+++ UnitTest2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void xUnitWorks2()
+        {
+            Assert.True(true);
+        }
+    }
+}
